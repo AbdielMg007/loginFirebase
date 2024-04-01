d92a05809a8ed8fe1849d600a74549fbc7e8652e
abdielmg007 <abdiel.mugo@gmail.com>
2024-04-01 14:39:10 -0600

    Initial Commit

---
+++ Multilogin/MultiloginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiloginApp.swift
+//  Multilogin
+//
+//  Created by Abdiel Mg on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiloginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
